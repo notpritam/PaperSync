4d0ce2c77d4891a858676fd3d304d64ac33e0ccc
notpritam <119423845+notpritam@users.noreply.github.com>
2023-08-24 02:36:29 +0530

    Design

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { uselogin } from "../../util/auth";
 import axios from "axios";
 import useUser from "../../util/store";
 import { useNavigate } from "react-router-dom";
@@ -23,7 +22,6 @@ function LoginPage() {
         {
           headers: {
             "Content-Type": "application/json",
-            // "x-access-token": token,
           },
         }
       )
@@ -42,10 +40,6 @@ function LoginPage() {
         console.log(error);
       });
   };
-
-  useEffect(() => {
-    // checkUser();
-  }, []);
 
   return (
     <div className="h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
@@ -108,20 +102,6 @@ function LoginPage() {
               </div>
               <div className="text-center sm:text-right whitespace-nowrap">
                 <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-4 h-4 inline-block align-text-bottom	"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
                   <span className="inline-block ml-1">Help</span>
                 </button>
               </div>
